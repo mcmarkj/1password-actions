739961e88b788ab24ca9a342d2b4471c31303ba4
mcmarkj <mark@markmcw.uk>
2021-05-26 10:24:58 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,56 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {OnePasswordConnect} from '@1password/connect'
+import * as parsing from './parsing'
+
+// Create new connector with HTTP Pooling
+const op = OnePasswordConnect({
+  serverURL: core.getInput('connect-server-url'),
+  token: core.getInput('connect-server-token'),
+  keepAlive: true
+})
+
+const getVaultID = async (vaultName: string): Promise<string> => {
+  const vaults = await op.listVaults()
+  for (const vault of vaults) {
+    if (vault.name === vaultName) {
+      return vault.id || ''
+    }
+  }
+  return ''
+}
+
+const getSecret = async (
+  vaultID: string,
+  secretTitle: string,
+  outputString: string
+): Promise<void> => {
+  const vaultItems = await op.getItemByTitle(vaultID, secretTitle)
+
+  const secretFields = vaultItems['fields'] || []
+  for (const items of secretFields) {
+    if (items.value != null) {
+      const outputName = `${outputString}_${items.id?.toLowerCase()}`
+      core.setSecret(items.value.toString())
+      core.exportVariable(outputName, items.value.toString())
+    }
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // Translate the vault path into it's respective segments
+    //const secretPath = core.getInput("secret-path")
+    const secretPath = 'marvin-secrets-development > anubis.github-credentials'
+    const itemRequests = parsing.parseItemRequestsInput(secretPath)
+    for (const itemRequest of itemRequests) {
+      // Get the vault ID for the vault
+      const secretVault = itemRequest.vault
+      const vaultID = getVaultID(secretVault)
+      // Set the secrets fields
+      const secretTitle = itemRequest.name
+      const outputString = itemRequest.outputName
+      getSecret(await vaultID, secretTitle, outputString)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
---
+++ src/parsing.js
@@ -0,0 +1,69 @@
+"use strict";
+exports.__esModule = true;
+exports.parseItemRequestsInput = void 0;
+/**
+ * Parses an item requests input string into vault names, item names and their resulting normalized output names.
+ * Based on parsing from https://github.com/hashicorp/vault-action/
+ */
+function parseItemRequestsInput(itemInput) {
+    var itemRequestLines = itemInput
+        .split('\n')
+        .filter(function (key) { return !!key; })
+        .map(function (key) { return key.trim(); })
+        .filter(function (key) { return key.length !== 0; });
+    var output = [];
+    for (var _i = 0, itemRequestLines_1 = itemRequestLines; _i < itemRequestLines_1.length; _i++) {
+        var itemRequestLine = itemRequestLines_1[_i];
+        var pathSpec = itemRequestLine;
+        var outputName = null;
+        var renameSigilIndex = itemRequestLine.lastIndexOf('|');
+        if (renameSigilIndex > -1) {
+            pathSpec = itemRequestLine.substring(0, renameSigilIndex).trim();
+            outputName = itemRequestLine.substring(renameSigilIndex + 1).trim();
+            if (outputName.length < 1) {
+                throw Error("You must provide a value when mapping an item to a name. Input: \"" + itemRequestLine + "\"");
+            }
+        }
+        var pathParts = [];
+        if (pathSpec.startsWith('"')) {
+            var secondQuoteIndex = pathSpec.indexOf('"', 1);
+            var vault_1 = pathSpec.substr(0, secondQuoteIndex + 1);
+            pathParts.push(vault_1);
+            // + 3 to remove the ' > ' prefix
+            var remainder = pathSpec.slice(secondQuoteIndex + 1 + 3);
+            pathParts.push(remainder);
+        }
+        else {
+            pathParts = pathSpec
+                .split(' > ')
+                .map(function (part) { return part.trim(); })
+                .filter(function (part) { return part.length !== 0; });
+        }
+        if (pathParts.length !== 2) {
+            throw Error("You must provide a valid vault and item name. Input: \"" + itemRequestLine + "\"");
+        }
+        var vaultQuoted = pathParts[0], nameQuoted = pathParts[1];
+        var vault = vaultQuoted.replace(new RegExp('"', 'g'), '');
+        var name_1 = nameQuoted.replace(new RegExp('"', 'g'), '');
+        if (!outputName) {
+            outputName = normalizeOutputName(name_1);
+        }
+        else {
+            outputName = normalizeOutputName(outputName);
+        }
+        output.push({
+            vault: vault,
+            name: name_1,
+            outputName: outputName
+        });
+    }
+    return output;
+}
+exports.parseItemRequestsInput = parseItemRequestsInput;
+function normalizeOutputName(dataKey) {
+    return dataKey
+        .replace(' ', '_')
+        .replace('.', '_')
+        .replace(/[^\p{L}\p{N}_-]/gu, '')
+        .toLowerCase();
+}
